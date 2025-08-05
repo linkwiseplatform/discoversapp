7d538e213eb0f2c9d4e1831b6db30a68a85c0386
강선주 <jkang@linkwise.kr>
2025-08-05 15:53:11 +0000

    app hosting에서 환경변수 설정을 찾다보니 "환경" 이라는 메뉴가 나왔는데 환경 이름을 관리하는 페이지야. "apphost

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 
 import type {NextConfig} from 'next';
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '.env.local' });
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -40,12 +37,6 @@ const nextConfig: NextConfig = {
     // This is required by Turbopack to serve static files.
     dirs: ['public'],
   },
-  env: {
-    NEXT_PUBLIC_KAKAO_REST_API_KEY: process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY,
-    KAKAO_CLIENT_SECRET: process.env.KAKAO_CLIENT_SECRET,
-    NEXT_PUBLIC_KAKAO_REDIRECT_URI: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI,
-    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
-  }
 };
 
 export default nextConfig;
